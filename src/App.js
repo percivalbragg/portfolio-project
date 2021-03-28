d3ada5438b88f46dad032c6cc8165d58c730ef8c
percivalbragg <percivalbragg@yahoo.com>
2021-03-27 21:41:53 -0500

    About Me completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 // import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
+import AboutMe from './components/AboutMe'
 
 function App() {
   return (
     <>
       <Navbar />
+      <AboutMe />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark" id="home">
             <div className="container">
                 <a className="navbar-brand" href="#"><img className='logo' src={logo} alt='logo...' /></a>
                 <button
@@ -18,10 +18,10 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" aria-current="page" href="#home">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">about me</a>
+                            <a className="nav-link" href="#about">about me</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">experience</a>
